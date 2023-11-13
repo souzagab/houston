ca7f0301ed62423d2ea3fa9d4198b6b7a387ee7a
Gabriel S. <thesouzagab@gmail.com>
2023-11-13 16:15:53 -0300

    chore: add bullet + annotate configurations

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,4 +51,10 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.raise         = true # raise an error if n+1 query occurs
+  end
 end
