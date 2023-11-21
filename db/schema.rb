5639e732df3421f8dd459acff9aa12b2de92f963
Gabriel S. <thesouzagab@gmail.com>
2023-11-21 19:29:20 -0300

    chore: create planet's table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_221457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planets", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "distance_to_earth", null: false, comment: "Distance in megameters (1mm = 1,000,000km)"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
