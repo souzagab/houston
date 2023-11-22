7bdd9c91c02699582f4c39ab776ae29298ccba5b
Gabriel S. <thesouzagab@gmail.com>
2023-11-21 22:02:23 -0300

    feat: add UFO model

---
+++ app/models/ufo.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: spacecrafts
+#
+#  id                                    :bigint           not null, primary key
+#  crew_capacity                         :integer          default(0), not null
+#  name                                  :string           not null
+#  remaining_fuel(Fuel capacity in days) :integer          not null
+#  speed(Speed in km/h)                  :float            not null
+#  type                                  :string           not null
+#  created_at                            :datetime         not null
+#  updated_at                            :datetime         not null
+#  agency_id                             :bigint           not null
+#
+# Indexes
+#
+#  index_spacecrafts_on_agency_id  (agency_id)
+#  index_spacecrafts_on_type       (type)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (agency_id => agencies.id)
+#
+class UFO < Spacecraft
+end
---
+++ spec/models/ufo_spec.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: spacecrafts
+#
+#  id                                    :bigint           not null, primary key
+#  crew_capacity                         :integer          default(0), not null
+#  name                                  :string           not null
+#  remaining_fuel(Fuel capacity in days) :integer          not null
+#  speed(Speed in km/h)                  :float            not null
+#  type                                  :string           not null
+#  created_at                            :datetime         not null
+#  updated_at                            :datetime         not null
+#  agency_id                             :bigint           not null
+#
+# Indexes
+#
+#  index_spacecrafts_on_agency_id  (agency_id)
+#  index_spacecrafts_on_type       (type)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (agency_id => agencies.id)
+#
+RSpec.describe UFO, type: :model do
+  it_behaves_like Spacecraft, :ufo
+end
