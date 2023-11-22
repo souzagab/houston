5d02d36ed003b6e2b7fb123f7df491530a3ede95
Gabriel S. <thesouzagab@gmail.com>
2023-11-21 22:54:10 -0300

    chore: create mission's table

---
+++ db/migrate/20231122011920_create_missions.rb
@@ -0,0 +1,16 @@
+class CreateMissions < ActiveRecord::Migration[7.1]
+  def change
+    create_enum :mission_status, %w[scheduled started canceled failed completed]
+
+    create_table :missions do |t|
+      t.references :planet, null: false, foreign_key: true
+      t.references :spacecraft, null: false, polymorphic: true
+      t.datetime :start_date, null: false
+      t.integer :duration, null: false, comment: "Duration in days"
+      t.text :description, null: false, default: "", limit: 500
+      t.enum :status, enum_type: :mission_status, default: "scheduled", null: false, index: true
+
+      t.timestamps
+    end
+  end
+end
