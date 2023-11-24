f4d876b3a0791608d6effda394c69201f5427e2a
Gabriel S. <thesouzagab@gmail.com>
2023-11-23 21:05:20 -0300

    chore: enable activejob

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -35,5 +35,7 @@ module Houston
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
