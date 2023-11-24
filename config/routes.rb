a613a3812b87de9f0b6e461207bd9d97da73eb83
Gabriel S <41161023+souzagab@users.noreply.github.com>
2023-11-23 21:06:31 -0300

    List missions of a spacecraft (#14)

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resources :missions
 
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :missions
+
+  resources :spacecrafts, only: [] do
+    resources :missions, only: :index, module: :spacecrafts
+  end
 end
---
+++ spec/requests/spacecrafts/missions_request_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe "spacecrafts/missions" do
+  describe "GET /index" do
+    context "when spacecraft does not exist" do
+      it "returns :not_found" do
+        get "/spacecrafts/0/missions"
+
+        expect(response).to have_http_status(:not_found)
+      end
+    end
+
+    context "when spacecraft exists" do
+      let!(:spacecraft) { create(%i[space_bus ufo rocket].sample) }
+      let!(:missions) { create_list(:mission, 3, spacecraft:) }
+
+      before { create_list(:mission, 3) }
+
+      it "returns only matching missions" do
+        get "/spacecrafts/#{spacecraft.id}/missions", params: { spacecraft_id: spacecraft.id }
+
+        expect(response).to have_http_status(:ok)
+        expect(response.parsed_body.size).to eq(missions.size)
+      end
+    end
+  end
+end
