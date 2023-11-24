d6465bc48e9fa6c291b04e171bbd00264a4e6931
Gabriel S. <thesouzagab@gmail.com>
2023-11-23 21:14:03 -0300

    chore: configure session for sidekiq webui

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  mount Sidekiq::Web => "/sidekiq"
-
   resources :missions
 
   resources :spacecrafts, only: [] do
