64330a6dcd347b81bc7c8db28f8e12af699e60c5
Gabriel S. <thesouzagab@gmail.com>
2023-11-23 21:07:46 -0300

    chore: enable sidekiq webui

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :missions
-
   get "up" => "rails/health#show", as: :rails_health_check
+
+  mount Sidekiq::Web => "/sidekiq"
 
   resources :missions
 
