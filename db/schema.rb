b26d603abf092f12074f631d76e0bcc6f4d5a1d2
Gabriel S. <thesouzagab@gmail.com>
2023-11-26 18:59:59 -0300

    chore: update schema with active storage tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_26_214259) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_26_214634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,34 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_26_214259) do
   # Note that some types may not work with other database engines. Be careful if changing database.
   create_enum "cargo_types", ["fuel", "trash", "probe", "satellite"]
   create_enum "mission_status", ["scheduled", "started", "canceled", "failed", "completed"]
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "agencies", force: :cascade do |t|
     t.string "name", null: false
@@ -81,6 +109,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_26_214259) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "missions", "planets"
   add_foreign_key "missions", "spacecrafts"
   add_foreign_key "payloads", "spacecrafts"
