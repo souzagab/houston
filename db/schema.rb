8a3e798135df579328beff40e6878804842f783a
Gabriel S. <thesouzagab@gmail.com>
2023-11-26 18:43:44 -0300

    chore: add versions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_23_000025) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_26_214259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_000025) do
     t.datetime "updated_at", null: false
     t.index ["agency_id"], name: "index_spacecrafts_on_agency_id"
     t.index ["type"], name: "index_spacecrafts_on_type"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "missions", "planets"
