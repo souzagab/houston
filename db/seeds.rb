e894e7afac6b4e59d18fc7098c950a8c7874318e
Gabriel S. <thesouzagab@gmail.com>
2023-11-28 17:00:00 -0300

    chore: add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,32 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
-# Create planets
-FactoryBot.create_list(:planet, 10)
+PLANETS = {
+  # AI Data, didn't confirm if the values are real
+  "Mercury" => 57.9,
+  "Venus" => 108.2,
+  "Mars" => 227.9,
+  "Jupiter" => 778.3,
+  "Saturn" => 1427.0,
+  "Uranus" => 2871.0,
+  "Neptune" => 4497.1,
+  "Pluto" => 5913.0
+}.freeze
 
-# Create agencies
-FactoryBot.create_list(:agency, 10)
+PLANETS.each do |name, distance_to_earth|
+  FactoryBot.create(:planet, name:, distance_to_earth:)
+end
+
+puts "Planets created"
+Planet.pluck(%i[id name]).each do |planet|
+  puts "  #{planet.last} (ID #{planet.first})"
+end
+
+Spacecraft::TYPES.each do |type|
+  FactoryBot.create(:spacecraft, type:)
+end
+
+puts "Spacecrafts created"
+Spacecraft.pluck(%i[id type]).each do |spacecraft|
+  puts "  #{spacecraft.last} (ID #{spacecraft.first})"
+end
